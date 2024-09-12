cba5dc05472a85adc5656b5ed652713d851f2f10
gudwin <gudwinnayak786@gmail.com>
2024-09-12 10:27:46 +0530

    hello world api -[]

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT=5000 | process.env.PORT 
+const greetings = require('./data.json');
+
+function generateID() {
+    return crypto.randomBytes(5).toString('hex');
+}
+app.get('/', (req, res) => {
+    res.send("Welcome to the Hello World API.");
+});
+app.get('/hello', (req, res) => {
+    const language = req.query.language
+    if (greetings[language]) {
+        const responseObject = {
+            msgText: greetings[language]
+        };
+        res.json(responseObject.msgText);
+        
+    } else {
+        const errorObject = {
+            error_message: "The requested language is not supported"
+        };
+        res.status(400).json(errorObject);
+    }
+});
+
+app.listen(5000, () => {
+
+    console.log(`Server is running on port ${PORT}`);
+});
